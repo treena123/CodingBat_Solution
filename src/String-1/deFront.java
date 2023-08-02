e1129c70f616396cb74b8ddb1535a305af729d06
treena123 <trina.pal1@t-mobile.com>
2023-08-02 08:10:31 -0700

    2nd August coding solution commit

--- src/String-1/deFront.java
+++ src/String-1/deFront.java
@@ -1,3 +1,12 @@
+/*
+
+Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+
+deFront("Hello") → "llo"
+deFront("java") → "va"
+deFront("away") → "aay"
+ */
 public String deFront(String str) {
         if(str.charAt(0) == 'a' && str.charAt(1)=='b'){
         return str;
